c1fa40890b5503f6f152ec63fcecdb98caeb6b71
suprayogo <126061696+suprayogo@users.noreply.github.com>
2023-09-06 13:55:41 +0700

    update UI page Home

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+     <>
+        <nav className="bg-gray-800 text-white w-full py-4">
+        <div className="container mx-auto">
+          <h1 className="text-2xl font-bold text-center">
+            Rock Paper Scissors Game
+          </h1>
+        </div>
+      </nav>
+     </>   
+    )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import "../style/animation.css";
+import Navbar from "../components/navbar";
 
 function Index() {
   return (
     <>
-      <nav className="bg-gray-800 text-white w-full py-4">
-        <div className="container mx-auto">
-          <h1 className="text-2xl font-bold text-center">
-            Rock Paper Scissors Game
-          </h1>
-        </div>
-      </nav>
-
+   < Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen animated-background">
-        <div className="container mx-auto mt-6">
-          <div className="flex flex-col items-center space-y-4">
+        <div className="container mx-auto mt-0">
+          <div className="flex flex-col items-center space-y-12">
+            <div className="relative">
+              <img
+                src="./logo200.png"
+                alt="Image Game"
+                className="w-full h-auto"
+                style={{ borderRadius: "10px" }}
+              />
+            </div>
             <Link href="/game?choice=rock">
               <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full">
                 Let's Go to Start
