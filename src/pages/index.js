36853ff43b2e16b1ac478f0d104bf491eeb373a1
suprayogo <126061696+suprayogo@users.noreply.github.com>
2023-09-06 14:41:28 +0700

    fix error img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image"; 
 import "../style/animation.css";
 import Navbar from "../components/navbar";
 
 function Index() {
   return (
     <>
-   < Navbar />
+      <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen animated-background">
         <div className="container mx-auto mt-0">
           <div className="flex flex-col items-center space-y-12">
             <div className="relative">
-              <img
-                src="./logo200.png"
+            
+              <Image
+                src="/logo200.png"
                 alt="Image Game"
-                className="w-full h-auto"
-                style={{ borderRadius: "10px" }}
+                width={200}
+                height={200}
+         
               />
             </div>
             <Link href="/game?choice=rock">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full">
-                Let's Go to Start
-              </button>
-            </Link>
+  <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full">
+    Let&apos;s Go to Start
+  </button>
+</Link>
+
           </div>
         </div>
       </div>
