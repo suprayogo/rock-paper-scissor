88567a25634cc6d2746fb138253effbe83bd57b6
suprayogo <126061696+suprayogo@users.noreply.github.com>
2023-09-06 14:32:35 +0700

    add sound music background

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
 function Navbar() {
-    return (
-     <>
-        <nav className="bg-gray-800 text-white w-full py-4">
-        <div className="container mx-auto">
-          <h1 className="text-2xl font-bold text-center">
-            Rock Paper Scissors Game
-          </h1>
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+   
+    if (audioRef.current) {
+    
+      audioRef.current.play();
+    } 
+  }, []);
+
+  return (
+    <>
+      <nav className="bg-gray-800 text-white w-full py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold text-center">Rock Paper Scissors Game</h1>
+        
+          <audio autoPlay loop ref={audioRef} src="/music-background.mp3" />
         </div>
       </nav>
-     </>   
-    )
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
